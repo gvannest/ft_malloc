f791c692cad2b3e2153332a742718dee804c47df
Charles-antoine PAQUET <cpaquet@e3r3p3.42.fr>
2019-12-28 10:47:41 +0100

    Add new variables to keep tiny/small ptr unmodified

--- src/search_free.c
+++ src/search_free.c
@@ -1,8 +1,14 @@
 
 #include "ft_malloc.h"
 
-void	*search_free(void *free_list, size_t size_user)
+void	*search_free(void *free_chuncks_list, size_t size_user)
 {
+// Declaration of a new ptr in order to keep tiny/small_chck unmodified
+	void*		free_list;
+
+	free_list = free_chuncks_list;
+// Checks the size of the space available in the differents headers 
+// of the different chucks in the list
 	while (free_list)
 	{
 		if (*((size_t*)(free_list + 8)) >= size_user)
