76a48a030c412a673713497e46a7f34d43ab05f3
Charles-antoine PAQUET <cpaquet@e3r5p3.42.fr>
2020-01-27 14:03:30 +0100

    Norme: large.c

--- src/large_chunk.c
+++ src/large_chunk.c
@@ -6,7 +6,7 @@
 /*   By: gvannest <gvannest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 15:46:19 by gvannest          #+#    #+#             */
-/*   Updated: 2020/01/17 15:10:05 by gvannest         ###   ########.fr       */
+/*   Updated: 2020/01/27 14:03:16 by cpaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 void	ft_large_allocation(void *ptr, size_t size)
 {
 	((t_chunk*)ptr)->mchunk_prevsize = 0;
-	((t_chunk*)ptr)->mchunk_size = ft_add_flags_to_size(size - HDR_HEAP - FTR_HEAP, 0, 1, 0);
+	((t_chunk*)ptr)->mchunk_size =
+	ft_add_flags_to_size(size - HDR_HEAP - FTR_HEAP, 0, 1, 0);
 	((t_chunk*)ptr)->prev_chunk = NULL;
 	((t_chunk*)ptr)->next_chunk = NULL;
 }
