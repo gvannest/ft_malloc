f47b1425feb17d7b5c2e08ccfbb40e692bbe2a79
Charles-antoine PAQUET <cpaquet@e3r5p3.42.fr>
2020-01-27 14:22:56 +0100

    norme: malloc_size.c

--- src/malloc_size.c
+++ src/malloc_size.c
@@ -1,25 +1,20 @@
-    //  The malloc_size() function returns the size of the memory block that
-    //  backs the allocation pointed to by ptr.  The memory block size is always
-    //  at least as large as the allocation it backs, and may be larger.
 
 #include "ft_malloc.h"
 
-size_t      malloc_size(const void *ptr)
- {
-    if (!find_current_heap((void*)ptr - HDR_SIZE))
-        return (0);
-     return (ft_size_wo_flags(((t_chunk*)(ptr - HDR_SIZE))->mchunk_size - HDR_SIZE));
- }
-
-
-    //  The malloc_good_size() function rounds size up to a value that the allo-
-    //  cator implementation can allocate without adding any padding; it then
-    //  returns that rounded-up value
-
-size_t      malloc_good_size(size_t size)
+size_t		malloc_size(const void *ptr)
 {
-    if (size < SMALL_MAX_SIZE)
-        return align_size(size);
-    else
-        return align_large_size(size);
+	size_t chunk_size;
+
+	chunk_size = ft_size_wo_flags(((t_chunk*)ptr)->mchunk_size - HDR_SIZE);
+	if (!find_current_heap((void*)ptr - HDR_SIZE))
+		return (0);
+	return (chunk_size - HDR_SIZE);
+}
+
+size_t		malloc_good_size(size_t size)
+{
+	if (size < SMALL_MAX_SIZE)
+		return (align_size(size));
+	else
+		return (align_large_size(size));
 }
